3ff88231969477180e78582e3e8a968cefd6a8e3
RediiChen <a0929052958@gmail.com>
2021-07-08 21:02:32 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace csharp_beginner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x=1, y=2 ;
+            int tNum ;
+            tNum = x ;
+            x = y ;
+            y = tNum ;
+            var sth = 'r' ;
+            Console.WriteLine("Say sth : " + sth) ;
+            Console.WriteLine("x = " + x + " and y = " + y);    
+        }
+    }
+}
